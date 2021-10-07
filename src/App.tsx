843c1d9f12f11787737bb9fdca54bbb8e31ead3a
Allen Garvey <allengarvey@hotmail.com>
2021-10-07 10:40:37 -0400

    Add results component to router

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,46 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  useParams,
+  useLocation,
 } from 'react-router-dom';
 import Search from './components/search';
+import Results from './components/results';
+
+interface ResultsParams {
+  userName: string;
+};
+
+interface ChildProps {
+  selectedGameTypes: string;
+}
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function Child({ selectedGameTypes }: ChildProps) {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  const { userName } = useParams<ResultsParams>();
+
+  return (
+    <Results userName={userName} selectedGameTypes={selectedGameTypes} />
+  );
+}
 
 function App() {
+  const query = useQuery();
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
           <Search />
         </Route>
+        <Route path="/u/:userName" children={<Child selectedGameTypes={query.get('gameTypes') || ''} />} />
       </Switch>
     </Router>
   );
