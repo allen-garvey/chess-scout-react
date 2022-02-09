c5f0390e4d212492270a24e6c21ee119c122ac3b
Allen Garvey <allengarvey@hotmail.com>
2022-02-09 08:07:20 -0500

    Remove underline from site title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => 
     <div className={css.container}>
         <h1 className={css.title}>
-            <Link to="/">Chess Scout</Link>
+            <Link to="/" className={css.link}>Chess Scout</Link>
         </h1>
     </div>;
 
